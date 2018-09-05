b6485818fa84011f74a3c42ed238cf5d7347ced0
Molly Drumm <m.drumm89@googlemail.com>
2018-09-05 14:42:23 +0100

    check films from particular year FALSE

--- specs/cinema_spec.js
+++ specs/cinema_spec.js
@@ -54,8 +54,13 @@ describe('Cinema', function () {
 
   });
 
+  it('should be able to check whether there are no films from a particular  year', function(){
+    const actual = cinema.isThereAMovieFrom(2015);
+    assert.strictEqual(actual, false);
+  });
 
-  it('should be able to check whether there are no films from a particular year');
+
+
   it('should be able to check whether all films are over a particular length');
   it('should be able to calculate total running time of all films');
 
