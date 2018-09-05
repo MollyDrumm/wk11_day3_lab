11a7f7d5e0785e6f710594e229502d703f91fa48
Molly Drumm <m.drumm89@googlemail.com>
2018-09-05 14:15:14 +0100

    addind find movie by title

--- models/cinema.js
+++ models/cinema.js
@@ -2,4 +2,23 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+
+
+Cinema.prototype.listOfAllFilmTitles = function(){
+        const result = this.films.map((film) => {  return film.title })
+        return result;
+}
+
+Cinema.prototype.findFilmByTitle = function(title){
+      let found = this.films.find(function(film){
+        if (film.title === title){
+          return film;
+        }
+      })
+    return found;
+}
+
+
+
+
 module.exports = Cinema;
